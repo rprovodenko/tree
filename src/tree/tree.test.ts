8727aa10ce6c430bec6e3f0bc109c3bdf69d40ef
Rostislav Provodenko <rostislav.provodenko@gmail.com>
2024-10-06 18:56:14 +0200

    wip

--- src/tree/tree.test.ts
+++ src/tree/tree.test.ts
@@ -2,13 +2,13 @@ import { initializeTree } from "./tree";
 /**
  * TODO:
  * - extra spaces, foward slashes at edges ?
+ * - error cases
+ * - moving where the same name exists
+ * - move to root
+ * - moving error cases
  */
 
 describe("tree navigator", () => {
-    it("finds node at path", () => {
-        // const treeNavigator = initializeTreeNavigator(tree)
-    })
-
     it("adds node at path - root level", () => {
         const tree = initializeTree();
         tree.addNode("child1")
@@ -268,18 +268,198 @@ Tree {
 `);
     })
 
-    // it("moves node at path1 to path2", () => {
-    //     const tree = initializeTree();
-    //     tree.addNode("asa/sad", "dasdas")
-    //     tree.addNode("adsads", "sadsadas")
-    //     tree.moveNode("asa/sad/dasdas", "adsads/sadsadas")
-    // })
+    it("finds node at path", () => {
+        const tree = initializeTree();
+        tree.addNode("child1a/child2/child3")
+        tree.addNode("child1b/child2/child3")
+        tree.addNode("child1c/child2/child3")
+        expect(tree.getNode("child1b/child2")).toMatchInlineSnapshot(`
+Node {
+  "childNodes": Map {
+    "child3" => Node {
+      "childNodes": Map {},
+      "hidden": false,
+      "name": "child3",
+    },
+  },
+  "hidden": false,
+  "name": "child2",
+}
+`);
+        expect(tree.getNode("child1c")).toMatchInlineSnapshot(`
+Node {
+  "childNodes": Map {
+    "child2" => Node {
+      "childNodes": Map {
+        "child3" => Node {
+          "childNodes": Map {},
+          "hidden": false,
+          "name": "child3",
+        },
+      },
+      "hidden": false,
+      "name": "child2",
+    },
+  },
+  "hidden": false,
+  "name": "child1c",
+}
+`);
+    })
 
-    // it("gets node at path", () => {
 
-    //     const tree = initializeTree();
-    //     tree.addNode("adsads", "sadsadas")
-    //     tree.deleteNode("adsads/sadsadas")
-    // })
-    
+    it("moves node at path1 to path2", () => {
+        const tree = initializeTree();
+        tree.addNode("child1a/child2a/child3")
+        tree.addNode("child1b/child2b/child3")
+        tree.moveNode("child1b/child2b/child3", "child1a/child2a/child3")
+        expect(tree).toMatchInlineSnapshot(`
+Tree {
+  "root": Node {
+    "childNodes": Map {
+      "child1a" => Node {
+        "childNodes": Map {
+          "child2a" => Node {
+            "childNodes": Map {
+              "child3" => Node {
+                "childNodes": Map {
+                  "child3" => Node {
+                    "childNodes": Map {},
+                    "hidden": false,
+                    "name": "child3",
+                  },
+                },
+                "hidden": false,
+                "name": "child3",
+              },
+            },
+            "hidden": false,
+            "name": "child2a",
+          },
+        },
+        "hidden": false,
+        "name": "child1a",
+      },
+      "child1b" => Node {
+        "childNodes": Map {
+          "child2b" => Node {
+            "childNodes": Map {},
+            "hidden": false,
+            "name": "child2b",
+          },
+        },
+        "hidden": false,
+        "name": "child1b",
+      },
+    },
+    "hidden": true,
+    "name": "root",
+  },
+}
+`);
+    })
+
+
+    it("moves nested dirs", () => {
+        const tree = initializeTree();
+        tree.addNode("child1a/child2a/child3")
+        tree.addNode("child1b/child2b/child3")
+        tree.moveNode("child1b/child2b", "child1a/child2a")
+        expect(tree).toMatchInlineSnapshot(`
+Tree {
+  "root": Node {
+    "childNodes": Map {
+      "child1a" => Node {
+        "childNodes": Map {
+          "child2a" => Node {
+            "childNodes": Map {
+              "child3" => Node {
+                "childNodes": Map {},
+                "hidden": false,
+                "name": "child3",
+              },
+              "child2b" => Node {
+                "childNodes": Map {
+                  "child3" => Node {
+                    "childNodes": Map {},
+                    "hidden": false,
+                    "name": "child3",
+                  },
+                },
+                "hidden": false,
+                "name": "child2b",
+              },
+            },
+            "hidden": false,
+            "name": "child2a",
+          },
+        },
+        "hidden": false,
+        "name": "child1a",
+      },
+      "child1b" => Node {
+        "childNodes": Map {},
+        "hidden": false,
+        "name": "child1b",
+      },
+    },
+    "hidden": true,
+    "name": "root",
+  },
+}
+`);
+    })
+
+
+    it("moves nested dirs - 2", () => {
+        const tree = initializeTree();
+        tree.addNode("child1a/child2a/child3")
+        tree.addNode("child1b/child2b/child3")
+        tree.moveNode("child1b", "child1a/child2a/child3")
+        expect(tree).toMatchInlineSnapshot(`
+Tree {
+  "root": Node {
+    "childNodes": Map {
+      "child1a" => Node {
+        "childNodes": Map {
+          "child2a" => Node {
+            "childNodes": Map {
+              "child3" => Node {
+                "childNodes": Map {
+                  "child1b" => Node {
+                    "childNodes": Map {
+                      "child2b" => Node {
+                        "childNodes": Map {
+                          "child3" => Node {
+                            "childNodes": Map {},
+                            "hidden": false,
+                            "name": "child3",
+                          },
+                        },
+                        "hidden": false,
+                        "name": "child2b",
+                      },
+                    },
+                    "hidden": false,
+                    "name": "child1b",
+                  },
+                },
+                "hidden": false,
+                "name": "child3",
+              },
+            },
+            "hidden": false,
+            "name": "child2a",
+          },
+        },
+        "hidden": false,
+        "name": "child1a",
+      },
+    },
+    "hidden": true,
+    "name": "root",
+  },
+}
+`);
+    })
 })
