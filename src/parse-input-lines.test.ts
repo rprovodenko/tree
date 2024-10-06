b7569a2df1fbf2c1490b63bdcafc9c3bc7f77a3c
Rostislav Provodenko <rostislav.provodenko@gmail.com>
2024-10-06 17:11:05 +0200

    rename

---
+++ src/parse-input-lines.test.ts
@@ -0,0 +1,9 @@
+import { parseInputLines } from "./parse-line/parse-input-line"
+
+describe("parseInputLines", () => {
+    it("parses CREATE", () => {
+        expect(parseInputLines("CREATE fruits")).toEqual({
+
+        })
+    })
+})
