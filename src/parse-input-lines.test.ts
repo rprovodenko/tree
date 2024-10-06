a6f04a8d40cec2d94916a7788891abe844e47edc
Rostislav Provodenko <rostislav.provodenko@gmail.com>
2024-10-06 17:11:50 +0200

    clean

--- src/parse-input-lines.test.ts
+++
@@ -1,9 +0,0 @@
-import { parseInputLines } from "./parse-line/parse-input-line"
-
-describe("parseInputLines", () => {
-    it("parses CREATE", () => {
-        expect(parseInputLines("CREATE fruits")).toEqual({
-
-        })
-    })
-})
