df44b4636bb6fbd5f5897d7bd7cce6649437e94f
rprovodenko <60982217+rprovodenko@users.noreply.github.com>
2024-10-07 00:43:45 +0200

    Edge cases (#8)

---
+++ src/apply-command-to-tree.ts
@@ -0,0 +1,20 @@
+import { CommandType, TreeCommand } from "./parse-line/command";
+import { Tree } from "./tree/tree";
+
+export function applyCommandToTree(tree: Tree, command: TreeCommand) {
+    if (command.type === CommandType.CREATE) {
+        tree.addNode(command.target);
+        return;
+    }
+    if (command.type === CommandType.DELETE) {
+        tree.removeNode(command.target);
+        return;
+    }
+    if (command.type === CommandType.MOVE) {
+        tree.moveNode(command.source, command.target);
+        return;
+    }
+    if (command.type === CommandType.LIST) {
+        return tree.list();
+    }
+}
--- src/multiline-feed.ts
+++ src/multiline-feed.ts
@@ -1,30 +1,12 @@
-import { TreeCommand, CommandType } from './parse-line/command';
+import { applyCommandToTree } from './apply-command-to-tree';
+import { CommandType } from './parse-line/command';
 import { parseInputLine } from './parse-line/parse-input-line';
 import { Tree } from './tree/tree';
 
 /**
- * TODO:
- * - list
- * - errors
+ * Note: this is not used right now, but can be used to run commands stored e.g. in a file
  */
 
-export function applyCommandToTree(tree: Tree, command: TreeCommand) {
-    if (command.type === CommandType.CREATE) {
-        tree.addNode(command.target);
-        return;
-    }
-    if (command.type === CommandType.DELETE) {
-        tree.removeNode(command.target);
-        return;
-    }
-    if (command.type === CommandType.MOVE) {
-        tree.moveNode(command.source, command.target);
-        return;
-    }
-    if (command.type === CommandType.LIST) {
-        return tree.list();
-    }
-}
 
 export function multilineFeed(tree: Tree, commands: string) {
     const lines = commands.split('\n');
